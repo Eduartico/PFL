811b1703dd82f0af61abbe5f628964a2a2e52c8f
Eduardo Duarte <duarte.eduardoeds@gmail.com>
2022-10-12 12:26:13 +0100

    Dou push

--- Trabalho/Trabalho.hs
+++ Trabalho/Trabalho.hs
@@ -1,4 +1,31 @@
-data M = M {num :: int | denom :: int | vars :: [string] | exps :: [int]}
+data M = M {num :: Float | vars :: [String] | exps :: [Int]}
 
 norm :: [M] -> [M]
 norm [] = []
+{-
+norm [] = []
+norm Antiga [x:xs]
+Nova insert (sumM (filter (\i xs, (x.vars == i.vars && x.exps == i.exps))))
+xs = filter (lista antiga, (x.vars != i.vars && x.exps != i.exps)) 
+Nova add (norm xs)
+-}
+
+sumM :: [M] -> M
+
+
+sortP :: [M] -> [M]
+
+
+multiM :: [M] -> [M]
+
+
+derivate :: [M] -> [M]
+
+
+{-
+matchM :: [M] -> Bool
+matchM P [] = False
+matchM P [x:xs]
+    | (x.vars == y.vars && x.exps == y.exps) = True
+    | Otherwise = False
+-}

5f219a0897c813c51610018877a6e78ecd81e8a2
Eduardo Duarte <duarte.eduardoeds@gmail.com>
2022-10-14 14:00:21 +0100

    tried multim

--- Trabalho/Trabalho.hs
+++ Trabalho/Trabalho.hs
@@ -10,6 +10,8 @@ xs = filter (lista antiga, (x.vars != i.vars && x.exps != i.exps))
 Nova add (norm xs)
 -}
 
+--import Data.List groupby e sortby
+
 sumM :: [M] -> M
 sumM [] = M{num = 0, vars = ['0'], exps = [0]}
 sumM (i:f) = M {num = num i + num (sumM f),vars = vars i,exps = exps i}
@@ -17,8 +19,9 @@ sumM (i:f) = M {num = num i + num (sumM f),vars = vars i,exps = exps i}
 --sortP :: [M] -> [M]
 
 
---multiM :: [M] -> [M]
-
+multiM :: [M] -> [M]
+multiM [] = M{num = 0, vars = ['0'], exps = [0]}
+multiM (i:f) = M {num = num i * num (sumM f),vars = vars i . vars (multiM f),exps = exps i . exps (multiM f)}
 
 --derivate :: [M] -> [M]
 

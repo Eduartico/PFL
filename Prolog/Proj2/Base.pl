fa6515ae9fe751b1e6a1f008cd4af0bfc7cf1193
Eduartico <duarte.eduardoeds@gmail.com>
2023-01-01 22:11:44 +0000

    push

--- Prolog/Proj2/Base.pl
+++ Prolog/Proj2/Base.pl
@@ -1,5 +1,5 @@
 :- use_module(library(lists)).
-:-dynamic board/1, color/1.
+:- dynamic board/1, color/1.
 startGame:- 
     writeLine('Select Game Mode:'), writeLine('1: Player vs Player'), writeLine('2: Player vs PC'), writeLine('3: PC vs PC'),
     read(GameMode),
@@ -27,7 +27,7 @@ startPVA:-
 
 % Support functions ---------------------------------------------
 
-printBoard(B):- maplist(writeLine, B).
+printBoard:- board(Board), maplist(writeLine, Board).
 
 writeLine(L):- writeq(L), nl.
 
@@ -36,17 +36,23 @@ clearScreen:- repeatNl(60).
 repeatNl(0).
 repeatNl(N):- N>0 ,nl, S is N-1, repeatNl(S).
 
+set_board(Value):-
+    retractall(board(_)), asserta(board(Value)).
+
+set_color(Value):-
+    retractall(color(_)), asserta(color(Value)).
+
 gameSetup:-
     Line = ['W','W','W','W','W'],
-    board = [Line, Line, Line, Line, Line],
-    nb_setval(board, Board1),
-    printBoard(board),
-    color = 'B'.
+    Board = [Line, Line, Line, Line, Line],
+    set_board(Board),
+    printBoard,
+    set_color('B').
 loopRefresh:-
-    (Color =:= 'B' -> Color = 'R';
-    Color = 'B'),
+    (color =:= 'B' -> color = 'R';
+    color = 'B'),
     B is 0, R is 0,
-    checkWin(Board)
+    %%checkWin,
     CT = 1, RT = 1.
 
 playerInput:-
@@ -59,23 +65,23 @@ getCoord(Coord, Message):- writeLine(Message), read(Coord1),
     ((integer(Coord1), Coord1 > 0 , Coord1 < 6) -> Coord = Coord1; 
     ((writeLine('Invalid Input!'), getCoord(Coord, Message)); true)).
 
-validMove(MoveRow, MoveCol):-
+/*validMove(MoveRow, MoveCol):-
     (MoveRow > 5 -> writeLine('Invalid Move!'); % todo condição
-    nth1(MoveRow, Board, X), nth1(Move, Board, X), startGame).
+    nth1(MoveRow, Board, X), nth1(Move, Board, X), startGame).*/
 
 makeMove(Row, Col):-
-    printBoard(Board),
-    read(Batata),
-    Board2 = board,
+    board(Board),
     B is 0, R is 0,
-    iterateMove(Row, Col, Board2, 1, B, R),
-    (B =:= R -> board = Board2; (writeLine('Invalid Move!'), playerInput) ; true).
+    iterateMove(Row, Col, Board, 1, B, R),
+    (B =:= R -> set_board(Board); (writeLine('Invalid Move!'), playerInput) ; true),
+    printBoard.
 
 iterateMove(Row, Col, [], I, B, R).
 
 iterateMove(Row, Col, [H|T], I, B, R):-
+    writeq(I), writeq('-'), writeq(Row), nl,
     ((I =:= Row + 1; I =:= Row - 1) -> checkRow(Col, H, 1, B, R);
-    Row =:= I -> iterateRow(Col, H, 1, B, R); true),
+    Row =:= I -> iterateRow(Col, H, 1, B, R); writeq('.'), nl) ; true,
     I1 is I+1,
     iterateMove(Row, Col, T, I1, B, R).
 
@@ -83,7 +89,7 @@ iterateRow(Col, [], J, B, R).
 
 iterateRow(Col, [H, T], J, B, R):-
     ((J =:= Col + 1; J =:= Col - 1) -> (H =:= 'R' -> R1 is R+1, R = R1; H =:= 'B' -> B1 is B+1, B = B1; true);
-    J =:= Col -> H = color; true),
+    J =:= Col -> color(Color), H = Color; true),
     J1 is J+1,
     iterateRow(Col, T, J1, B, R).
 
@@ -95,8 +101,17 @@ checkRow(Col, [H, T], J, B, R):-
     checkRow(Col, T, J1, B, R)).
 
 
+/*
+checkWin:-
+Btotal = 0, Rtotal = 0,
+iterateMoveRec(ColT, RowT, 1, B, R),
+Btotal 
 
-checkWin(ColT, RowT):-
-iterateMove(ColT, RowT, Board, 1, B, R),
+somaRec(0, 0).                   
+somaRec(N, F):- N > 0,
+N1 is N-1,
+somaRec(N1, F1), 
+F is F1 + N.
+
 (CT =:= 5; RT =:= 5 -> 
-Color = 'B'),
+Color = 'B').*/
